4a36a1ed57786ac2b45f772e36ccd2e4673fde15
Nikita Spirianov <sna2021@narod.ru>
2021-07-05 22:09:13 +0300

    Update stars.dart

--- lib/widget/stars.dart
+++ lib/widget/stars.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 class Stars extends StatelessWidget {
   const Stars({Key key}) : super(key: key);
 
+  
   @override
   Widget build(BuildContext context) {
+    //MARK: можно использовать const для всех SizedBox
     return Container(
       color: Color(0xFF1A1927),
       padding: EdgeInsets.symmetric(horizontal: 36),
       child: Column(
-        children: [
+        children: [ 
+          // MARK: Можно использовать vertical отступ в padding, тогда и Column не нужен. 
           SizedBox(
             height: 24,
           ),
           Row(
             children: [
+             //MARK: Нужно использовать Flexible, Иначе будут проблемы на маленьких экранах
               SizedBox(
                 height: 24,
               ),
@@ -43,7 +47,9 @@ class Stars extends StatelessWidget {
   }
 }
 
+// MARK: Название метода должно содержать в себе действие
 Widget circleStarFilled() {
+  // MARK: Лучше сделать через Container + BoxDecoration
   return Stack(
     children: [
       CircleAvatar(backgroundColor: Colors.grey, radius: 27),
@@ -63,6 +69,7 @@ Widget circleStarFilled() {
 }
 
 Widget circleStarNotFilled() {
+// MARK: Лучше сделать через Container + BoxDecoration
   return Stack(
     children: [
       CircleAvatar(backgroundColor: Colors.grey, radius: 27),
